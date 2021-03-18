b288c20fcf7a4dd2ce2735ea09b647bede15dea9
Amith <amithdas89@gmail.com>
2021-03-18 15:19:43 +0530

    Testing Jenkins Poll

--- src/main/java/amith/java/JavaApplication.java
+++ src/main/java/amith/java/JavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Jenkins poll test");
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
